7df0879bde2014efbb53ec49726e2931d62c561a
chenjkai <chenjk@ctrip.com>
2019-06-08 09:28:46 +0800

    bk

---
+++ src/main/java/com/chenjk/test/concurrent/ReentrantReadWriteLockTest.java
@@ -0,0 +1,16 @@
+package com.chenjk.test.concurrent;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author chenjkai
+ * @date 2019/6/6
+ */
+public class ReentrantReadWriteLockTest {
+
+    ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    public void test() {
+        
+    }
+}
